a6d9185d3cb6ef4e290ac0a0821a4b291a80d423
mdbonnke <111636754+mdbonnke@users.noreply.github.com>
2026-03-08 07:38:27 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/arunga-health-insights/",
   server: {
     host: "::",
     port: 8080,
